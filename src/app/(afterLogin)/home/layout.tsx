9919219996757d4e3be8344bbce4d5d729826b2a
seungheon1029 <seungheon1029@gmail.com>
2024-01-29 13:46:19 +0900

    feat: grouping으로 인한 folder structure 변경

---
+++ src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,5 @@
+export default function HomeLayout({children}) {
+  return (
+    <div>홈 레이아웃 {children}</div>
+  )
+}
---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      안녕하세요 홈입니다.
+    </div>
+  )
+}
