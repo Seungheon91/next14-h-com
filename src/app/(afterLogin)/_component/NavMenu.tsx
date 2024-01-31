c999d40e452930c44da3454eb3dc4845f137a183
seungheon1029 <seungheon1029@gmail.com>
2024-01-31 17:37:51 +0900

    feat: afterLogin layout 구성

---
+++ src/app/(afterLogin)/_component/NavMenu.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { useSelectedLayoutSegment } from 'next/navigation';
+
+export default function NavMenu() {
+  const segment = useSelectedLayoutSegment();
+  const me = {};
+
+  return <div>ddd dsf</div>;
+}
